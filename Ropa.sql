14f4d3e13b38f979dc253367386816f9f7c34722
javierromualdo2006 <javieragustinromualdo2006@gmail.com>
2024-09-17 17:06:47 -0300

    corregimos la sintaxis de las tablas y creamos los usuarios y permisos, ademas el comando para crear el backup

--- Ropa.sql
+++ Ropa.sql
@@ -1,46 +1,63 @@
+comando backup
+
+mysqldump -h 10.9.120.5 -P 3307 -u ropauba -p ropauba > ropauba_backup.sql
+
+
+CREATE USER 'DanielCufos'@'%' IDENTIFIED BY 'ropauba';
+CREATE USER 'javierromualdo2006'@'%' IDENTIFIED BY 'ropauba';
+
+GRANT ALL PRIVILEGES ON ropauba.* TO 'DanielCufos'@'%';
+GRANT ALL PRIVILEGES ON ropauba.* TO 'javierromualdo2006'@'%';
+
 CREATE TABLE Publicaciones (
-id INT PRIMARY KEY,
-id_Usuarios INT FOREIGN KEY,
-id_Productos INT FOREIGN KEY,
-Titulo VARCHAR(50),
-Imagen VARCHAR(200),
-Descripcion TEXT
+    id INT PRIMARY KEY,
+    id_Usuarios INT,
+    id_Productos INT,
+    Titulo VARCHAR(50),
+    Imagen VARCHAR(200),
+    Descripcion TEXT,
+    FOREIGN KEY (id_Usuarios) REFERENCES Usuarios(id),
+    FOREIGN KEY (id_Productos) REFERENCES Productos(id)
 );
 
 CREATE TABLE Usuarios (
-id INT PRIMARY KEY,
-Nombre VARCHAR(50),
-Apellido VARCHAR(50),
-Correo VARCHAR(200),
-Numero_de_Telefono INT,
-Direccion VARCHAR(200)
+    id INT PRIMARY KEY,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+    Correo VARCHAR(200),
+    Numero_de_Telefono INT,
+    Direccion VARCHAR(200)
 );
 
 CREATE TABLE Productos (
-id INT PRIMARY KEY,
-Nombre VARCHAR(50),
-Cantidad INT,
-Precio FLOAT,
-Stock INT
+    id INT PRIMARY KEY,
+    Nombre VARCHAR(50),
+    Cantidad INT,
+    Precio FLOAT,
+    Stock INT
 );
 
 CREATE TABLE Compras (
-id INT PRIMARY KEY,
-id_Productos INT FOREIGN KEY,
-id_Usuarios INT FOREIGN KEY,
-Estado BOOLEAN,
-Metodo_de_pago INT,
-Hora_Entrega TIME,
-Hora_Compra TIME
+    id INT PRIMARY KEY,
+    id_Productos INT,
+    id_Usuarios INT,
+    Estado BOOLEAN,
+    Metodo_de_pago INT,
+    Hora_Entrega TIME,
+    Hora_Compra TIME,
+    FOREIGN KEY (id_Productos) REFERENCES Productos(id),
+    FOREIGN KEY (id_Usuarios) REFERENCES Usuarios(id)
 );
 
 CREATE TABLE UsuariosRoles (
-id INT PRIMARY KEY,
-id_Usuario INT FOREIGN KEY,
-id_Roles INT FOREIGN KEY
+    id INT PRIMARY KEY,
+    id_Usuario INT,
+    id_Roles INT,
+    FOREIGN KEY (id_Usuario) REFERENCES Usuarios(id),
+    FOREIGN KEY (id_Roles) REFERENCES Roles(id)
 );
 
 CREATE TABLE Roles (
-id INT PRIMARY KEY,
-Nombre_Rol VARCHAR(50),
+    id INT PRIMARY KEY,
+    Nombre_Rol VARCHAR(50)
 );
