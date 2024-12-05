e4f4530b807f70e7bcf0ebaa9053466f40364a03
DanielCufos <danielcufos111@gmail.com>
2024-12-05 12:05:52 -0300

    actividad iniciada

---
+++ guardar code.sql
@@ -0,0 +1,6 @@
+INSERT INTO `Productos`(`id`, `Nombre`, `Cantidad`, `Precio`, `Stock`) VALUES ('9','[Camisa de Guille]','2','11990','2')
+
+INSERT INTO `Publicaciones`(`id`, `id_Usuarios`, `id_Productos`, `Titulo`, `Imagen`, `Descripcion`) VALUES ('20', '3', '9', 'Camisa de Guille', 'https://www.abrafersrl.com.ar/wp-content/uploads/RCA-000450.jpg', 'Camisa de Guillermo Cuneo');
+
+------------------
+
